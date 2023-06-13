c4cd3dd6abb4e687a694b800529aa6ca278a3b8d
DataNerdTommy <136404152+DataNerdTommy@users.noreply.github.com>
2023-06-13 17:08:24 -0400

    bite 19 - add a simple property to a class
    added a simple property to class

---
+++ 19/simple_property.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+NOW = datetime.now()
+
+
+class Promo:
+    def __init__(self, name, expires) -> None:
+        self.name = name
+        self.expires = expires
+    
+    @property
+    def expired(self):
+        if self.expires < NOW:
+            return True
+        else:
+            return False
